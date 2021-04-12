95ce5f2409b455f729c17c33bacf6e0fc065e974
Ian-CM <30392014+broopa@users.noreply.github.com>
2021-04-12 12:08:53 -0400

    Basic WIKI search & display

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,21 +6,44 @@ const Search = () => {
     const [results, setResults] = useState([]);
 
     useEffect(() => {
-        (async () =>  
-         { const { data } = { await axios.get('https://en.wikipedia.org/w/api.php', {
-            params: {
-                action:'query',
-                list: 'search',
-                origin: '*',
-                format: 'json',
-                srsearch: search,
-            }
-        });
-    })();
+        const wikisearch = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: search,
+                },
+            });
+            setResults(data.query.search);
+        };
 
-    setResults(data.query.search);
+        if(search) {
+            wikisearch();
+        }
+    
+    }, [search]);
 
-    }};, [search]);
+    const renderedResults = results.map((results) => {
+        return (
+            <div className="item" key={results.pageid}>
+                <div className="right floated contnet">
+                    <a className="ui button" 
+                    href={`https://en.wikipedia.org?curid=${results.pageid}`}
+                    >Read More</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {results.title}
+                    </div>
+                    <div className="content">
+                        <p dangerouslySetInnerHTML={{__html: results.snippet}}></p>
+                    </div>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -32,8 +55,11 @@ const Search = () => {
                     className="input" placeholder="Enter Search Term" />
                 </div>
             </div>
+            <div className="ui celled list">
+            {renderedResults}
+            </div>
         </div>
     );
-}
+};
 
 export default Search;

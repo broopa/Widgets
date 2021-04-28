e3ba2cc6333ca926bb09821fdf74a38b1d8a4c55
Ian-CM <30392014+broopa@users.noreply.github.com>
2021-04-28 17:21:09 -0400

    Added Dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+
+const options = [
+    { 
+        label: 'Color Red',
+        value: 'red'
+    },
+    {
+        label: 'Color Blue',
+        value: 'blue'
+    },
+    {
+        label: 'Color Green',
+        value: 'green'
+    }
+];
+
 
 const items =[
     {
@@ -19,9 +36,18 @@ const items =[
 
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]); 
+    const [showDropdown, setShowDropdown] = useState(true);
+
+
     return (
         <div>
-            <Search />
+         {/* <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button> */}
+            {showDropdown ? 
+                <Dropdown
+            selected={selected}
+            onSelectedChange={setSelected}
+            options={options} /> : null }
         </div>
     );
 };
